394655185ce304220b5a239ba616362147a1bd70
arunmathaisk <arunmathaisk@gmail.com>
2020-10-01 13:11:12 +0530

    small change in decprecated module

--- ip.js
+++ ip.js
@@ -1,5 +1,10 @@
 const publicIp = require('public-ip');
- 
+
 (async () => {
     console.log(await publicIp.v4());
 })();
+
+
+
+
+//npm install express cookie-parser morgan mysql2 helmet compression express-session express-mysql-session csurf passport passport-local debug
