cd1e562163450480349e402bf7edeee3ab68a458
OMKAR AGRAWAL <Omkaragrawal@users.noreply.github.com>
2021-02-16 21:31:02 +0530

    added all the SQLs

---
+++ sql/addComp.procedure.sql
@@ -0,0 +1,6 @@
+create procedure addCOMP(IN ac int, IN cname varchar(50), IN descrip varchar(200), IN logo varchar(325))
+BEGIN
+insert into `company`(`active`,`name`,`description`,`logo`) values(ac,cname,descrip,logo);
+Set @status='Company details added';
+Select @status;
+END;
---
+++ sql/company.table.sql
@@ -0,0 +1,9 @@
+create table company
+(
+	id int(10) auto_increment
+		primary key,
+	active int not null,
+	name varchar(50) not null,
+	description varchar(200) not null,
+	logo varchar(325) not null
+);
